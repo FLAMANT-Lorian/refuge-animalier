d92efab9cb1f925a8aa766e3d49c23b80387ac4e
FLAMANT-Lorian <lorian.flamant@student.hepl.be>
2025-11-12 21:53:53 +0100

    remove obsolete attribute

--- resources/views/components/layouts/partials/header.blade.php
+++ resources/views/components/layouts/partials/header.blade.php
@@ -12,7 +12,7 @@
 
         {{--BURGER MENU CLOSE ??? => block peer-checked:hidden --}}
         <label for="burger-menu" class="cursor-pointer">
-            <svg class="" width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <svg class="" width="32" height="32" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
                 <rect x="4" y="7" width="24" height="2" rx="1" fill="#292A2B"/>
                 <rect x="7" y="15" width="18" height="2" rx="1" fill="#292A2B"/>
                 <rect x="4" y="23" width="24" height="2" rx="1" fill="#292A2B"/>
