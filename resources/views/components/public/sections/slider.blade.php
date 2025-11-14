8a4a6bad12545aeaf96d47e15781bcc73661c47a
FLAMANT-Lorian <lorian.flamant@student.hepl.be>
2025-11-14 09:08:51 +0100

    installed AplineJS

--- resources/views/components/public/sections/slider.blade.php
+++ resources/views/components/public/sections/slider.blade.php
@@ -2,13 +2,13 @@
     'title' => 'Titre',
     'articles' => []
 ])
-<section>
+<section class="px-6">
     <h2 class="text-2xl font-bold text-center">
         {!! $title !!}
     </h2>
 
     @if($articles)
-        <div class="slider_selector grid grid-cols-[repeat(3,327px)] gap-[6rem] px-6 overflow-x-scroll">
+        <div class="slider_selector grid grid-cols-[repeat(3,327px)] gap-[6rem] overflow-x-scroll">
             @foreach($articles as $idx => $article)
 
                 <article class="slider_item_selector pt-8 relative flex flex-col items-center">
