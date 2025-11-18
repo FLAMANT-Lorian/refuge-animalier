594e985e2b120c92e1016a5757e54f1a11abfcb6
FLAMANT-Lorian <lorian.flamant@student.hepl.be>
2025-11-18 17:36:09 +0100

    text-media media-queries

--- resources/views/components/public/sections/text-media.blade.php
+++ resources/views/components/public/sections/text-media.blade.php
@@ -10,9 +10,10 @@
    'btn_destination'
 ])
 
-<section class="py-[4.5rem] px-6 flex gap-12
-{!! $text_location === 'left' ? 'flex-col' : 'flex-col-reverse' !!}">
-    <div class="flex flex-col gap-6">
+<section class="py-[4.5rem] md:py-[6rem] lg:py-[11rem] px-6 md:px-12 lg:px-[12rem] flex md:grid md:grid-cols-10 gap-6 lg:grid-cols-[repeat(13,minmax(0,1fr))] md:items-center
+{!! $text_location === 'left' ? 'flex-col' : 'flex-col-reverse' !!}
+md:{!! $text_location === 'left' ? 'flex-row' : 'flex-row-reverse' !!}">
+    <div class="flex flex-col gap-6 md:col-start-1 md:col-end-6 lg:col-end-7">
         <div>
             <h2 class="text-2xl font-bold pb-1">
                 {!! $title !!}
@@ -28,7 +29,7 @@
             {!! $btn_label !!}
         </x-public.button>
     </div>
-    <picture>
-        <img src="{!! $img_path !!}" alt="{!! $img_alt !!}">
+    <picture class="rounded-2xl md:col-start-6 lg:col-start-8 md:col-end-11 lg:col-end-[14]">
+        <img class="rounded-2xl w-full" src="{!! $img_path !!}" alt="{!! $img_alt !!}">
     </picture>
 </section>
