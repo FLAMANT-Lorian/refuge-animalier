bfe4942a34b711a3fe86ae3b18fa9dd26dc4e50f
FLAMANT-Lorian <lorian.flamant@student.hepl.be>
2025-11-19 12:11:49 +0100

    fix burger menu

--- resources/views/components/public/navigation/burger-menu-cross.blade.php
+++ resources/views/components/public/navigation/burger-menu-cross.blade.php
@@ -1,6 +1,6 @@
 <input class="hidden bg-menu" type="checkbox" name="bg-menu" id="burger-menu">
 <label for="burger-menu" class="cursor-pointer bg-menu-label md:hidden">
-    <svg class="flex peer-checked:hidden" width="32" height="32" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
+    <svg width="32" height="32" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
         <rect x="4" y="7" width="24" height="2" rx="1" fill="#292A2B"/>
         <rect x="7" y="15" width="18" height="2" rx="1" fill="#292A2B"/>
         <rect x="4" y="23" width="24" height="2" rx="1" fill="#292A2B"/>
