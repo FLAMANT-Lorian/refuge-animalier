90537e0fc74c5c556a7449ccf4e55d571c33fd49
FLAMANT-Lorian <lorian.flamant@student.hepl.be>
2025-11-19 20:20:37 +0100

    refactor animals index page in components

---
+++ resources/views/components/public/sections/animals.blade.php
@@ -0,0 +1,13 @@
+<section class="px-6 py-[4.5rem] md:px-12 md:py-[6rem] lg:px-[12rem] lg:pb-[11rem]">
+    <div class="flex flex-col gap-6">
+        <h2 class="text-2xl font-bold">
+            Nos animaux
+        </h2>
+
+        <x-forms.input-search
+            name="search-field"
+            id="search-field"
+            placeholder="Rechercher un animal"
+            label="Rechercher un animal"/>
+    </div>
+</section>
