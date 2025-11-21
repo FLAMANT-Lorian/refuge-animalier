f80acf115d667024da39a876fc9a1d2b0c27d9a5
FLAMANT-Lorian <lorian.flamant@student.hepl.be>
2025-11-21 11:18:47 +0100

    published lang files

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,18 @@
 <?php
 
+use App\Http\Controllers\AnimalController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function (){
+Route::get('/', function () {
     return view('public.homepage');
 })->name('public.homepage');
 
-Route::get('/a-propos', function (){
+Route::get('/a-propos', function () {
     return view('public.pages.about');
 })->name('public.about');
 
-Route::get('/contact', function (){
+Route::get('/contact', function () {
     return view('public.pages.contact');
 })->name('public.contact');
 
-Route::get('/nos-animaux', function (){
-    return view('public.animals.index');
-})->name('public.animals.index');
+Route::get('/nos-animaux', [AnimalController::class, 'index'])->name('public.animals.index');
