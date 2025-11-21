7500e7d97d714b824a62429c3655eb52f5573a74
FLAMANT-Lorian <lorian.flamant@student.hepl.be>
2025-11-21 13:32:18 +0100

    added arrow anchor

--- resources/views/components/public/sections/hero.blade.php
+++ resources/views/components/public/sections/hero.blade.php
@@ -10,6 +10,11 @@
             plus sur son caractère, son âge et ses besoins, et trouver celui qui vous correspond le mieux&nbsp;!
         </p>
     </div>
+    <a class="transition-all absolute bottom-10 left-1/2 -translate-x-1/2 hover:translate-y-2" href="#animals">
+        <svg width="20" height="34" viewBox="0 0 20 34" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M8.64202 1.35373C8.64202 0.606088 9.25 0 10 0C10.75 0 11.358 0.606088 11.358 1.35373V29.3785L17.682 23.0743C18.2123 22.5456 19.0719 22.5456 19.6023 23.0743C20.1326 23.6029 20.1326 24.4599 19.6023 24.9885L10.9601 33.6035C10.4298 34.1322 9.57019 34.1322 9.03986 33.6035L0.397744 24.9885C-0.132581 24.4599 -0.132581 23.6029 0.397744 23.0743C0.92807 22.5456 1.78769 22.5456 2.31802 23.0743L8.64202 29.3785V1.35373Z" fill="#FBFCFA"/>
+        </svg>
+    </a>
     <figure
         class="absolute block -top-[6.75rem] md:-top-[7.375rem] -z-[2] h-[37.5rem] lg:h-[56.25rem] w-full max-w-[110rem] before:absolute before:w-full before:max-w-[110rem]  before:h-full before:content-[''] before:bg-[rgba(17,17,17,0.45)] before:top-0 before:bottom-0 before:right-0 before:left-0">
         <img class="hero w-full h-full object-cover max-h-full"
