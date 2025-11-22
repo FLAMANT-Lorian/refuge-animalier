0901f43c2f6af2003024a56e162d04141abd035e
FLAMANT-Lorian <lorian.flamant@student.hepl.be>
2025-11-22 09:28:50 +0100

    create subfolders for svg in img folder

--- resources/views/components/forms/input-search.blade.php
+++ resources/views/components/forms/input-search.blade.php
@@ -10,11 +10,7 @@
         {!! $label !!}
     </label>
 
-    <svg class="absolute fill-transparent right-4 top-1/2 -translate-y-1/2"
-        width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-        <path d="M16.927 17.04L20.4001 20.4M19.2801 11.44C19.2801 15.7699 15.77 19.28 11.4401 19.28C7.11019 19.28 3.6001 15.7699 3.6001 11.44C3.6001 7.11006 7.11019 3.59998 11.4401 3.59998C15.77 3.59998 19.2801 7.11006 19.2801 11.44Z" stroke="#57A770" stroke-width="2" stroke-linecap="round"/>
-    </svg>
-
+    <x-icons.search class="absolute fill-transparent right-4 top-1/2 -translate-y-1/2"/>
     <input
         class="w-full items-start bg-white transition-all pl-4 pr-[3.5rem] py-3 outline outline-green-500 rounded-lg placeholder:text-gray-500"
         placeholder="{!! $placeholder ?? '' !!}"
