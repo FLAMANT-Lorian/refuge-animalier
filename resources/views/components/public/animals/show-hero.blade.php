e9ecda62bb0cdb030c2d1f10802f62ed5f8ec4d7
FLAMANT-Lorian <lorian.flamant@student.hepl.be>
2025-11-22 09:15:05 +0100

    add copy to clipboard script to copy the url when the shared button is pressed

--- resources/views/components/public/animals/show-hero.blade.php
+++ resources/views/components/public/animals/show-hero.blade.php
@@ -3,18 +3,19 @@
 ])
 
 <div class="flex flex-col gap-6 mb-8">
-    <div class="flex gap-4 justify-between items-center">
+    <div class="relative flex gap-4 justify-between items-center">
         <div class="flex flex-wrap items-center gap-4">
             <h3 class="text-3xl font-bold">
                 {!! $animal->name !!}
             </h3>
             <x-animals.state :animal_state="$animal->state"/>
         </div>
-        <a class="flex flex-row gap-2 transition-all p-2 rounded-lg border border-transparent hover:border-gray-100 hover:bg-white"
+        <a class="share_button flex flex-row gap-2 transition-all p-2 rounded-lg border border-transparent hover:border-gray-100 hover:bg-white"
            href="{!! url()->current() !!}">
             Partager
             <x-icons.share/>
         </a>
+        <span class="opacity-0 transition-all ease-in-out duration-100 share_message absolute right-0 -top-0.5 -rotate-2 font-bold">Copier !</span>
     </div>
     <p>
         {!! $animal->description !!}
