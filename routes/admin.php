fa148a152c187886aeefc706fc237a9b333676dd
FLAMANT-Lorian <lorian.flamant@student.hepl.be>
2025-11-26 10:19:45 +0100

    change admin route slug

--- routes/admin.php
+++ routes/admin.php
@@ -1,23 +1,23 @@
 <?php
 
-Route::livewire('/login', 'pages::login')->name('admin.login');
+Route::livewire('/admin/login', 'pages::login')->name('admin.login');
 
-Route::livewire('/dashboard', 'pages::dashboard')->name('admin.dashboard');
+Route::livewire('/admin/dashboard', 'pages::dashboard')->name('admin.dashboard');
 
-Route::livewire('/settings', 'pages::settings')->name('admin.settings');
+Route::livewire('/admin/settings', 'pages::settings')->name('admin.settings');
 
-Route::livewire('/animals', 'pages::animals.index')->name('admin.animals.index');
-Route::livewire('/animals/create', 'pages::animals.create')->name('admin.animals.create');
-Route::livewire('/animals/{animal}/edit', 'pages::animals.edit')->name('admin.animals.edit');
-Route::livewire('/animals/{animal}', 'pages::animals.show')->name('admin.animals.show');
+Route::livewire('/admin/animals', 'pages::animals.index')->name('admin.animals.index');
+Route::livewire('/admin/animals/create', 'pages::animals.create')->name('admin.animals.create');
+Route::livewire('/admin/animals/{animal}/edit', 'pages::animals.edit')->name('admin.animals.edit');
+Route::livewire('/admin/animals/{animal}', 'pages::animals.show')->name('admin.animals.show');
 
-Route::livewire('/messages', 'pages::messages.index')->name('admin.messages.index');
+Route::livewire('/admin/messages', 'pages::messages.index')->name('admin.messages.index');
 
-Route::livewire('/volunteers', 'pages::volunteers.index')->name('admin.volunteers.index');
-Route::livewire('/volunteers/create', 'pages::volunteers.create')->name('admin.volunteers.create');
-// Route::livewire('/volunteers/{volunteer}', 'pages::volunteers.show')->name('admin.volunteers.show');
-Route::livewire('/volunteers/1', 'pages::volunteers.show')->name('admin.volunteers.show');
+Route::livewire('/admin/volunteers', 'pages::volunteers.index')->name('admin.volunteers.index');
+Route::livewire('/admin/volunteers/create', 'pages::volunteers.create')->name('admin.volunteers.create');
+// Route::livewire('/admin/volunteers/{volunteer}', 'pages::volunteers.show')->name('admin.volunteers.show');
+Route::livewire('/admin/volunteers/1', 'pages::volunteers.show')->name('admin.volunteers.show');
 
-Route::livewire('/adoption-requests', 'pages::adoption-requests.index')->name('admin.adoption-requests.index');
+Route::livewire('/admin/adoption-requests', 'pages::adoption-requests.index')->name('admin.adoption-requests.index');
 
-Route::livewire('/animal-sheets', 'pages::animal-sheets.index')->name('admin.animal-sheets.index');
+Route::livewire('/admin/animal-sheets', 'pages::animal-sheets.index')->name('admin.animal-sheets.index');
