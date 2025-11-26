204f8f1fe9ff3de2ce0af3822fbc14e312af2e73
FLAMANT-Lorian <lorian.flamant@student.hepl.be>
2025-11-26 10:19:36 +0100

    Create login form component

---
+++ resources/views/components/admin/login/login-form.blade.php
@@ -0,0 +1,26 @@
+<form action="" method="post" class="flex flex-col gap-5 w-full">
+
+    {{-- INPUT EMAIL --}}
+    <x-forms.input-text
+        id="email"
+        name="email"
+        type="email"
+        label="Adresse e-mail"
+        placeholder="johndoe@example.be"
+        :required="true"/>
+
+    {{-- INPUT PASSWORD --}}
+    <div class="flex flex-col gap-2">
+        <x-forms.input-password
+            id="password"
+            name="password"
+            label="Mot de passe"
+            :required="true"/>
+        <a href="#" class="text-blue-600 hover:underline self-end">
+            Mot de passe oublié ?
+        </a>
+    </div>
+
+    {{-- INPUT SUBMIT --}}
+    <x-forms.button-submit label="Me connecter"/>
+</form>
