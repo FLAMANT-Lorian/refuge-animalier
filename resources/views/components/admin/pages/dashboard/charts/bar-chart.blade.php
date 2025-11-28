3dc0688298bf0e145258ae1fb7e2ff9e5d3142f3
FLAMANT-Lorian <lorian.flamant@student.hepl.be>
2025-11-28 09:31:21 +0100

    init base script for each chart

--- resources/views/components/admin/pages/dashboard/charts/bar-chart.blade.php
+++ resources/views/components/admin/pages/dashboard/charts/bar-chart.blade.php
@@ -1,17 +1,40 @@
-<canvas id="barChart" width="200" height="200" class="border border-green-300 rounded-2xl bg-gray-50"></canvas>
+<canvas id="barChart" width="200" height="200" class="border border-green-300 rounded-2xl bg-gray-50 p-4"></canvas>
 
 @script
 <script>
-    document.addEventListener('livewire:init', () => {
+
+    document.addEventListener('livewire:initialized', () => {
         const barCHart = {
-            canvas: document.getElementById('barChart'),
+            ctx: document.getElementById('barChart'),
 
             init() {
                 this.displayChart();
             },
 
             displayChart() {
-
+                new Chart(this.ctx, {
+                    type: 'bar',
+                    data: {
+                        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                        datasets: [{
+                            label: '# of Votes',
+                            data: [12, 19, 3, 5, 2, 3],
+                            borderWidth: 1
+                        }]
+                    },
+                    options: {
+                        plugins: {
+                            legend: {
+                                display: false,
+                            }
+                        },
+                        scales: {
+                            y: {
+                                beginAtZero: true
+                            }
+                        }
+                    }
+                });
             }
         }
         barCHart.init();
--- resources/views/components/admin/pages/dashboard/charts/doughnut-chart.blade.php
+++ resources/views/components/admin/pages/dashboard/charts/doughnut-chart.blade.php
@@ -1,17 +1,39 @@
-<canvas id="doughnutChart" width="200" height="200" class="border border-green-300 rounded-2xl bg-gray-50"></canvas>
+<canvas id="doughnutChart" width="200" height="200" class="border border-green-300 rounded-2xl bg-gray-50 p-4"></canvas>
 
 @script
 <script>
-    document.addEventListener('livewire:init', () => {
+    document.addEventListener('livewire:initialized', () => {
         const doughnutChart = {
-            canvas: document.getElementById('doughnutChart'),
+            ctx: document.getElementById('doughnutChart'),
 
             init() {
                 this.displayChart();
             },
 
             displayChart() {
-
+                new Chart(this.ctx, {
+                    type: 'doughnut',
+                    data: {
+                        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                        datasets: [{
+                            label: '# of Votes',
+                            data: [12, 19, 3, 5, 2, 3],
+                            borderWidth: 1
+                        }]
+                    },
+                    options: {
+                        plugins: {
+                            legend: {
+                                display: false,
+                            }
+                        },
+                        scales: {
+                            y: {
+                                beginAtZero: true
+                            }
+                        }
+                    }
+                });
             }
         }
         doughnutChart.init();
