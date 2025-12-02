96c520480dc120451ad499ed938414dd751caa08
FLAMANT-Lorian <lorian.flamant@student.hepl.be>
2025-12-02 17:35:31 +0100

    fix bug in url

--- resources/views/pages/animals/⚡index/index.blade.php
+++ resources/views/pages/animals/⚡index/index.blade.php
@@ -11,7 +11,7 @@
 
             {{-- FIL D'ARIANE --}}
             <a wire:navigate href="{!! route('admin.animals.index') !!}"
-               class="font-bold text-gray-500">| {!! $app_title !!}</a>
+               class="self-start font-bold text-gray-500">| {!! $app_title !!}</a>
 
             <div class="flex flex-col lg:grid lg:grid-cols-9 lg:items-center lg:gap-6 gap-6">
                 <div class="flex flex-col gap-2 lg:col-start-1 lg:col-end-5">
