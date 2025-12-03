3a8c21174afb24c81ee147495b38f2b3c114e010
FLAMANT-Lorian <lorian.flamant@student.hepl.be>
2025-12-03 15:12:26 +0100

    Added table for animal-sheets index

---
+++ resources/views/components/admin/pages/animal-sheets/index/table.blade.php
@@ -0,0 +1,18 @@
+@props([
+    'animal_sheets'
+])
+
+<section {!! $attributes->merge(['class' => 'flex flex-col gap-6']) !!}>
+    <table class="lg:overflow-hidden lg:border-separate lg:rounded-2xl lg:border lg:border-green-300 lg:border-spacing-0">
+
+        {{-- THEAD --}}
+        <x-admin.pages.animal-sheets.index.thead/>
+
+        {{-- TBODY --}}
+        <x-admin.pages.animal-sheets.index.tbody :animal_sheets="$animal_sheets"/>
+
+    </table>
+
+    {{--  PAGINATION --}}
+    {{--{!! $volunteers->links('vendor.livewire.tailwind', ['results_name' => '$volunteers']) !!}--}}
+</section>
--- resources/views/pages/animal-sheets/⚡index/index.php
+++ resources/views/pages/animal-sheets/⚡index/index.php
@@ -1,8 +1,27 @@
 <?php
 
+use App\Enums\SheetsStatus;
+use Livewire\Attributes\Computed;
+use Livewire\Attributes\Title;
 use Livewire\Component;
+use Livewire\WithPagination;
 
-new class extends Component
-{
-    //
+new #[Title('Gestion des fiches · Les pattes heureuses')]
+class extends Component {
+
+    use WithPagination;
+
+    public string $app_title = 'Gestion des fiches';
+
+    #[Computed]
+    public function animal_sheets(): array
+    {
+        return [
+            'name' => 'Flamant Lorian',
+            'date' => '30 octobre 2026',
+            'volunteer' => 'Flamant Lorian',
+            'status' => SheetsStatus::Creation->value
+        ];
+
+    }
 };
