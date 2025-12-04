6bbce67529fdb6c0fce87e438a8e88c30796ea48
FLAMANT-Lorian <lorian.flamant@student.hepl.be>
2025-12-04 10:56:59 +0100

    added a button to add a volunteer

--- resources/views/pages/volunteers/⚡index/index.blade.php
+++ resources/views/pages/volunteers/⚡index/index.blade.php
@@ -21,6 +21,14 @@
 
                 <div class="flex flex-col md:flex-row gap-4 lg:col-start-5 lg:col-end-10 lg:justify-end lg:items-center lg:gap-6">
 
+                    {{-- BOUTON POUR AJOUTER UN BÉNÉVOLE --}}
+                    <x-buttons.add-item-button
+                        class="self-start md:order-2 md:ml-auto lg:m-0"
+                        title="Ajouter un bénévole"
+                        :href="route('admin.volunteers.create')">
+                        Ajouter un bénévole
+                    </x-buttons.add-item-button>
+
                     {{-- CHAMPS DE RECHERCHE --}}
                     <x-forms.fields.input-search
                         class="md:order-1"
