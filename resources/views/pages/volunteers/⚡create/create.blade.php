4ffc1757e37687328879257d38c7105ba62fdac1
FLAMANT-Lorian <lorian.flamant@student.hepl.be>
2025-12-06 15:11:43 +0100

    fix padding

--- resources/views/pages/volunteers/⚡create/create.blade.php
+++ resources/views/pages/volunteers/⚡create/create.blade.php
@@ -1,4 +1,4 @@
-<main class="volunteer-create flex-1 px-6 md:px-12 py-12 " id="content">
+<main class="volunteer-create flex-1 px-6 py-12 md:px-12 lg:px-16 lg:py-10" id="content">
     <div class="flex flex-col gap-10 lg:bg-white lg:border lg:border-gray-200 lg:rounded-2xl lg:p-6">
 
         {{-- EN-TÊTE --}}
--- resources/views/pages/⚡settings/settings.blade.php
+++ resources/views/pages/⚡settings/settings.blade.php
@@ -1,4 +1,4 @@
-<main class="settings flex-1 px-6 md:px-12 py-12 flex flex-col gap-6" id="content">
+<main class="settings flex-1 px-6 py-12 md:px-12 lg:px-16 lg:py-10 flex flex-col gap-6" id="content">
     <div class="flex flex-col gap-10 lg:bg-white lg:border lg:border-gray-200 lg:rounded-2xl lg:p-6">
 
         {{-- EN-TÊTE --}}
