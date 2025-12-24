cea4665bfb3196e2ef6a4098a1545f28d38e637a
FLAMANT-Lorian <lorian.flamant@student.hepl.be>
2025-12-24 16:13:57 +0100

    testing message class

--- resources/views/pages/messages/⚡index/index.test.php
+++ resources/views/pages/messages/⚡index/index.test.php
@@ -1,13 +1,18 @@
 <?php
 
+use App\Enums\MessageStatus;
+use App\Enums\UserStatus;
 use App\Models\Message;
 use App\Models\User;
 use Livewire\Livewire;
 use function Pest\Laravel\actingAs;
 
-describe('CONNECTED USER', function () {
+describe('ADMIN USER', function () {
     beforeEach(function () {
-        $this->user = User::factory()->create();
+        $this->user = User::factory()->create([
+            'role' => UserStatus::Admin->value
+        ]);
+
         actingAs($this->user);
     });
 
@@ -19,11 +24,49 @@ describe('CONNECTED USER', function () {
 
     it('verifies if a user can see all messages',
         function () {
-
             $message = Message::factory()->create();
 
             Livewire::test('pages::messages.index')
                 ->assertSee($message->email);
         }
     );
+
+    it('verifies if a an admin can change the status of a message in not-read', function () {
+        $message = Message::factory()->create();
+
+        Livewire::test('pages::messages.index')
+            ->call('openModal', 'message', $message->id)
+            ->call('markAsNotRead', $message->id);
+
+        $message = Message::first();
+        expect($message->status)->toBe(MessageStatus::Unread->value);
+    });
+
+    it('verifies if a an admin can change the status of a message in read', function () {
+        $message = Message::factory()->create(
+            ['status' => MessageStatus::Unread->value]
+        );
+
+        Livewire::test('pages::messages.index')
+            ->call('openModal', 'message', $message->id);
+
+        $message = Message::first();
+        expect($message->status)->toBe(MessageStatus::Read->value);
+    });
+});
+
+describe('VOLUNTEER USER', function () {
+    beforeEach(function () {
+        $this->user = User::factory()->create([
+            'role' => UserStatus::Volunteer->value
+        ]);
+
+        actingAs($this->user);
+    });
+
+    it('verifies if a you can’t access to the admin messages index page', function () {
+
+        Livewire::test('pages::messages.index')
+            ->assertForbidden();
+    });
 });
