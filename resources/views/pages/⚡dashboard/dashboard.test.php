da5c261cb8fad6d87826d594db2a952d90af4c2e
FLAMANT-Lorian <lorian.flamant@student.hepl.be>
2025-12-28 18:23:42 +0100

    fix bug

--- resources/views/pages/⚡dashboard/dashboard.test.php
+++ resources/views/pages/⚡dashboard/dashboard.test.php
@@ -1,8 +1,18 @@
 <?php
 
+use App\Models\User;
 use Livewire\Livewire;
+use function Pest\Laravel\actingAs;
 
-it('verifies if a you can access to the admin dashboard page', function () {
-    Livewire::test('pages::dashboard')
-        ->assertStatus(200);
+describe('CONNECTED USER', function () {
+    beforeEach(function () {
+        $this->user = User::factory()->create();
+
+        actingAs($this->user);
+    });
+
+    it('verifies if a you can access to the admin dashboard page', function () {
+        Livewire::test('pages::dashboard')
+            ->assertStatus(200);
+    });
 });
