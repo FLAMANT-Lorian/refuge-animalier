6b6c88b0a07aa485c50aef48d75a76c553959e6f
FLAMANT-Lorian <lorian.flamant@student.hepl.be>
2025-12-30 19:41:12 +0100

    handle language selector in public website

--- resources/views/components/public/navigation/header/navigation-links.blade.php
+++ resources/views/components/public/navigation/header/navigation-links.blade.php
@@ -1,6 +1,6 @@
 @php
 
-    $items = [
+    use App\Enums\AvailableLanguage;$items = [
         ['label'=> __('public/header.link_1'), 'title' => __('public/header.link_1_title'), 'destination' => route('public.animals.index'), 'class' => 'text-2xl md:text-base font-normal md:font-medium hover:font-bold active:font-bold transition-all'],
         ['label'=> __('public/header.link_2'), 'title' => __('public/header.link_2_title'), 'destination' => route('public.about'), 'class'=> 'text-2xl md:text-base font-normal md:font-medium hover:font-bold active:font-bold transition-all'],
         ['label'=> __('public/header.link_3'), 'title' => __('public/header.link_3_title'), 'destination' => route('public.contact'), 'class' => 'text-2xl md:text-base font-normal md:font-medium hover:font-bold active:font-bold transition-all md:last:py-2.5 md:last:px-4 md:last:bg-green-500 md:last:text-white md:last:rounded-lg md:last:hover:font-medium md:last:hover:bg-transparent md:last:hover:text-black md:last:border md:border-green-500'],
@@ -11,12 +11,20 @@
 <ul class="flex flex-col gap-6 md:flex-row md:items-center md:justify-end">
     @foreach($items as $item)
         <li class="flex">
-            <x-public.navigation.header.navigation-link
-                :destination="$item['destination']"
-                :title="$item['title']"
-                :class="$item['class']">
+            <x-public.navigation.header.navigation-link :destination="$item['destination']" :title="$item['title']"
+                                                        :class="$item['class']">
                 {!! $item['label'] !!}
             </x-public.navigation.header.navigation-link>
         </li>
     @endforeach
+
+    <li class="max-md:mt-auto ">
+        @foreach(AvailableLanguage::cases() as $locale)
+            @if($locale->value !== app()->getLocale())
+                <a title="Changer la langue en {{ __('enum.' . $locale->value) }}"
+                   class="text-2xl md:text-base font-normal md:font-medium hover:font-bold active:font-bold trans-all"
+                   href="{{ route('public.locale', $locale->value) }}">{{ __('enum.' . $locale->value) }}</a>
+            @endif
+        @endforeach
+    </li>
 </ul>
